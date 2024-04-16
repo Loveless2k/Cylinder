46593c733611f232d6a4dc76e661891e7c8c2db7
Jorge Salgado <j.salgado2k@gmail.com>
2024-04-16 16:26:49 -0400

    First Commit

---
+++ src/Cylinder.java
@@ -0,0 +1,16 @@
+public class Cylinder extends Circle{
+
+    private double height;
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = Math.max(height, 0);
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume(){
+        return getArea() * height;
+    }
+}
